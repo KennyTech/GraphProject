023881074271811a20159e37187c3af76bb0fb8e
Elllion <32142429+Elllion@users.noreply.github.com>
2018-03-31 15:21:51 -0400

    Initial Connection
    Project
    -Moved to its own folder
    -Can connect to a server
    
    Server
    -Accepts clients and tells them their "ID"

---
+++ Graph Server/src/main/java/Main.java
@@ -0,0 +1,106 @@
+//package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javafx.scene.chart.XYChart.Series;
+import javafx.collections.ObservableList;
+
+class ConnectionHandler extends Thread{
+	Socket clientSocket;
+	
+	public ConnectionHandler(Socket connect){
+		clientSocket = connect;
+	}
+	public void run(){		
+        int i = 0;
+		 
+		try{
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			String line = Integer.toString(Main.connectedClients.size());
+			out.println(line);
+			System.out.println("Line Sent");
+			out.flush();
+			
+			
+		 
+			if(Main.connectedClients.size() == 1){
+				
+			}
+			else{
+				
+			}
+			
+             
+			 
+			 out.print(line);
+			
+			
+          InputStream cin = clientSocket.getInputStream();
+          InputStreamReader creader = new InputStreamReader(cin);
+          BufferedReader cbin = new BufferedReader(creader);
+		  while(i > 0){
+          String cline = null;
+
+          //Wait for client's command
+				if ((cline = cbin.readLine()) != null) {
+					System.out.println(cline);
+					cline = null;
+				}
+			
+		  }
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
+public class Main{
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+	
+	public static ArrayList<Socket> connectedClients = new ArrayList<>();
+	public static ObservableList<Series<Float, Float>> seriesList;
+	
+    public static void main(String [] args) {
+        try {
+            serverSocket = new ServerSocket(8888);
+
+            System.out.println("Ready");
+
+            //Constantly accept client connections
+            while(true){
+
+                clientSocket = serverSocket.accept();
+                if(clientSocket != null){
+					ConnectionHandler ch = new ConnectionHandler(clientSocket);			
+					ch.start();
+					
+					connectedClients.add(clientSocket);
+					System.out.println(connectedClients.size());
+					sendMessage(clientSocket);
+                }
+				clientSocket = null;
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+	
+	public static void sendMessage(Socket sender){
+		for(Socket c: connectedClients){
+			if(c != sender){
+					System.out.println("HI");
+
+			}
+		}
+	}
+}
